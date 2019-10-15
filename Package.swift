be4a4f46ccd11371c7d32d688bdc147f2303dad9
Adam Fowler <adamfowler71@gmail.com>
2019-10-15 10:29:53 +0100

    Update to v3.3.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
-  name: "SwiftAWSEcs",
+  name: "ECS",
   products: [
-      .library(name: "SwiftAWSEcs", targets: ["SwiftAWSEcs"]),
+      .library(name: "ECS", targets: ["ECS"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.4.0"))
   ],
   targets: [
-      .target(name: "SwiftAWSEcs", dependencies: ["AWSSDKSwiftCore"]),
+      .target(name: "ECS", dependencies: ["AWSSDKSwiftCore"]),
   ]
 )
